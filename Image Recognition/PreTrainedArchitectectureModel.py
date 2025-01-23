1585f44a73c6a65113866ae0bc3c953639ea020d
NoahSantaub <nsantaub@gmail.com>
2025-01-23 16:28:52 +0000

    Improved File Management & maintainability

---
+++ Image Recognition/PreTrainedArchitectectureModel.py
@@ -0,0 +1,370 @@
+# Load the libraries
+import tensorflow as tf
+print(tf.__version__) #verifies tensorflow version
+print(tf.config.list_physical_devices('GPU')) #checks for gpu
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import os
+import cv2
+import matplotlib.pyplot as plt
+import random
+import time
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.model_selection import train_test_split
+import keras
+print(keras.__version__)
+print(tf.keras.__version__)#version check
+from tensorflow.python.keras import Sequential
+from tensorflow.python.keras.layers import Activation, Dropout, Flatten, Dense, Conv2D, MaxPooling2D
+from tensorflow.python.keras.utils.np_utils import to_categorical
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+import gc
+from IPython.display import Markdown, display
+from time import perf_counter
+#import kagglehub
+
+def printmd(string):
+    # Print with Markdowns    
+    display(Markdown(string))
+
+np.random.seed(0) # Add random seed of training for reproducibility
+
+def load_images_from_folder(folder,only_path = False, label = ""):
+# Load the paths to the images in a directory
+# or load the images
+    if only_path == False:
+        images = []
+        for filename in os.listdir(folder):
+            img = plt.imread(os.path.join(folder,filename))
+            if img is not None:
+                images.append(img)
+        return images
+    else:
+        path = []
+        for filename in os.listdir(folder):
+            img_path = os.path.join(folder,filename)
+            if img_path is not None:
+                path.append([label,img_path])
+        return path
+
+# Load the paths on the images
+images = []
+directory = r"C:\Users\nsant\OneDrive\Documents\Uni\Y3\Project_MMME3083\Code\Fruit Image DB"
+for f in os.listdir(directory):
+    if "png" in os.listdir(directory+'\\'+f)[0]:
+        images += load_images_from_folder(directory+'\\'+f,True,label = f)
+    else: 
+        for d in os.listdir(directory+'\\'+f):
+            images += load_images_from_folder(directory+"\\"+f+"\\"+d,True,label = f)
+            
+# Create a dataframe with the paths and the label for each fruit
+df = pd.DataFrame(images, columns = ["fruit", "path"])
+
+# Shuffle the dataset
+from sklearn.utils import shuffle
+df = shuffle(df, random_state = 0)
+df = df.reset_index(drop=True)
+
+# Assign to each fruit a specific number
+fruit_names = sorted(df.fruit.unique())
+mapper_fruit_names = dict(zip(fruit_names, [t for t in range(len(fruit_names))]))
+df["label"] = df["fruit"].map(mapper_fruit_names)
+#print(mapper_fruit_names)
+
+"""
+# Visualize the resulting dataframe
+df.head()
+# Display the number of pictures of each category
+vc = df["fruit"].value_counts()
+plt.figure(figsize=(10,5))
+sns.barplot(x = vc.index, y = vc, palette = "rocket")
+plt.title("Number of pictures of each category", fontsize = 15)
+plt.xticks(rotation=90)
+plt.show()
+
+# Display some pictures of the dataset
+fig, axes = plt.subplots(nrows=4, ncols=5, figsize=(15, 15),subplot_kw={'xticks': [], 'yticks': []})
+for i, ax in enumerate(axes.flat):
+    ax.imshow(plt.imread(df.path[i]))
+    ax.set_title(df.fruit[i], fontsize = 12)
+plt.tight_layout(pad=0.0)
+plt.show()
+"""
+
+#!  Train the neural network from scratch with Keras and w/o generator
+# The pictures will be resized to have the same size for the neural network
+
+img = plt.imread(df.path[0])
+plt.imshow(img)
+plt.title("Original image")
+plt.show()
+
+plt.imshow(cv2.resize(img, (150,150)))
+plt.title("After resizing")
+plt.show()
+
+
+#!Competition of 27 pre-trained architectures - May the best win
+# Use only 5% on the pictures to speed up the training
+train_df,test_df = train_test_split(df[['path','fruit']].sample(frac=0.05,random_state=0), test_size=0.2,random_state=0)
+def create_gen():
+    # Load the Images with a generator and Data Augmentation
+    train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input,
+        validation_split=0.1
+    )
+
+    test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+        preprocessing_function=tf.keras.applications.mobilenet_v2.preprocess_input
+    )
+
+    train_images = train_generator.flow_from_dataframe(
+        dataframe=train_df,
+        x_col='path',
+        y_col='fruit',
+        target_size=(224, 224),
+        color_mode='rgb',
+        class_mode='categorical',
+        batch_size=32,
+        shuffle=True,
+        seed=0,
+        subset='training',
+    #   rotation_range=30, # Uncomment those lines to use data augmentation
+    #   zoom_range=0.15,
+    #   width_shift_range=0.2,
+    #   height_shift_range=0.2,
+    #   shear_range=0.15,
+    #   horizontal_flip=True,
+    #   fill_mode="nearest"
+    )
+
+    val_images = train_generator.flow_from_dataframe(
+        dataframe=train_df,
+        x_col='path',
+        y_col='fruit',
+        target_size=(224, 224),
+        color_mode='rgb',
+        class_mode='categorical',
+        batch_size=32,
+        shuffle=True,
+        seed=0,
+        subset='validation',
+    #   rotation_range=30, # Uncomment those lines to use data augmentation
+    #   zoom_range=0.15,
+    #   width_shift_range=0.2,
+    #   height_shift_range=0.2,
+    #   shear_range=0.15,
+    #   horizontal_flip=True,
+    #   fill_mode="nearest"
+    )
+
+    test_images = test_generator.flow_from_dataframe(
+        dataframe=test_df,
+        x_col='path',
+        y_col='fruit',
+        target_size=(224, 224),
+        color_mode='rgb',
+        class_mode='categorical',
+        batch_size=32,
+        shuffle=False
+    )
+    
+    return train_generator,test_generator,train_images,val_images,test_images
+
+def get_model(model):
+# Load the pretained model
+    kwargs =    {'input_shape':(224, 224, 3),
+                'include_top':False,
+                'weights':'imagenet',
+                'pooling':'avg'}
+    
+    pretrained_model = model(**kwargs)
+    pretrained_model.trainable = False
+    
+    inputs = pretrained_model.input
+
+    x = tf.keras.layers.Dense(128, activation='relu')(pretrained_model.output)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+
+    outputs = tf.keras.layers.Dense(15, activation='softmax')(x)
+
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+    model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+    return model
+
+# Dictionary with the models
+models = {
+    "DenseNet121": {"model":tf.keras.applications.DenseNet121, "perf":0},
+    "MobileNetV2": {"model":tf.keras.applications.MobileNetV2, "perf":0},
+    "DenseNet169": {"model":tf.keras.applications.DenseNet169, "perf":0},
+    "DenseNet201": {"model":tf.keras.applications.DenseNet201, "perf":0},
+    "EfficientNetB0": {"model":tf.keras.applications.EfficientNetB0, "perf":0},
+    "EfficientNetB1": {"model":tf.keras.applications.EfficientNetB1, "perf":0},
+    "EfficientNetB2": {"model":tf.keras.applications.EfficientNetB2, "perf":0},
+    "EfficientNetB3": {"model":tf.keras.applications.EfficientNetB3, "perf":0},
+    "EfficientNetB4": {"model":tf.keras.applications.EfficientNetB4, "perf":0},
+    "EfficientNetB5": {"model":tf.keras.applications.EfficientNetB4, "perf":0},
+    "EfficientNetB6": {"model":tf.keras.applications.EfficientNetB4, "perf":0},
+    "EfficientNetB7": {"model":tf.keras.applications.EfficientNetB4, "perf":0},
+    #"InceptionResNetV2": {"model":tf.keras.applications.InceptionResNetV2, "perf":0},#inocrrect link / wrong model unable to be called
+    "InceptionV3": {"model":tf.keras.applications.InceptionV3, "perf":0},
+    "MobileNet": {"model":tf.keras.applications.MobileNet, "perf":0},
+    "MobileNetV2": {"model":tf.keras.applications.MobileNetV2, "perf":0},
+    "MobileNetV3Large": {"model":tf.keras.applications.MobileNetV3Large, "perf":0},
+    "MobileNetV3Small": {"model":tf.keras.applications.MobileNetV3Small, "perf":0},
+    #"NASNetLarge": {"model":tf.keras.applications.NASNetLarge, "perf":0}, Deleted because the input shape has to be another one
+    "NASNetMobile": {"model":tf.keras.applications.NASNetMobile, "perf":0},
+    "ResNet101": {"model":tf.keras.applications.ResNet101, "perf":0},
+    "ResNet101V2": {"model":tf.keras.applications.ResNet101V2, "perf":0},
+    "ResNet152": {"model":tf.keras.applications.ResNet152, "perf":0},
+    "ResNet152V2": {"model":tf.keras.applications.ResNet152V2, "perf":0},
+    "ResNet50": {"model":tf.keras.applications.ResNet50, "perf":0},
+    "ResNet50V2": {"model":tf.keras.applications.ResNet50V2, "perf":0},
+    "VGG16": {"model":tf.keras.applications.VGG16, "perf":0},
+    "VGG19": {"model":tf.keras.applications.VGG19, "perf":0},
+    "Xception": {"model":tf.keras.applications.Xception, "perf":0}
+}
+
+# Create the generators
+train_generator,test_generator,train_images,val_images,test_images=create_gen()
+
+# Fit the models
+for name, model in models.items():
+    
+    # Get the model
+    m = get_model(model['model'])
+    models[name]['model'] = m
+    
+    start = perf_counter()
+    
+    # Fit the model
+    history = m.fit(train_images,validation_data=val_images,epochs=1,verbose=0)
+    
+    # Sav the duration and the val_accuracy
+    duration = perf_counter() - start
+    duration = round(duration,2)
+    models[name]['perf'] = duration
+    print(f"{name:20} trained in {duration} sec")
+    
+    val_acc = history.history['val_accuracy']
+    models[name]['val_acc'] = [round(v,4) for v in val_acc]
+
+
+for name, model in models.items():
+    
+    # Predict the label of the test_images
+    predict = models[name]['model'].predict(test_images)
+    predict = np.argmax(predict,axis=1)
+
+    # Map the label
+    labels = (train_images.class_indices)
+    labels = dict((v,k) for k,v in labels.items())
+    predict = [labels[k] for k in predict]
+
+    y_test = list(test_df.fruit)
+    acc = accuracy_score(y_test,predict)
+    models[name]['acc'] = round(acc,4)
+#     printmd(f'**{name} has a {acc * 100:.2f}% accuracy on the test set**')
+
+# Create a DataFrame with the results
+models_result = []
+
+for name, v in models.items():
+    models_result.append([ name, models[name]['val_acc'][-1],models[name]['acc'],models[name]['perf']])
+    
+df_results = pd.DataFrame(models_result,columns = ['model','val_accuracy','accuracy','Training time (sec)'])
+df_results.sort_values(by='accuracy', ascending=False, inplace=True)
+df_results.reset_index(inplace=True,drop=True)
+df_results
+
+plt.figure(figsize = (15,5))
+sns.barplot(x = 'model', y = 'accuracy', data = df_results)
+plt.title('Accuracy on the test set (after 1 epoch))', fontsize = 15)
+plt.ylim(0,1)
+plt.xticks(rotation=90)
+plt.show()
+
+plt.figure(figsize = (15,5))
+sns.barplot(x = 'model', y = 'Training time (sec)', data = df_results)
+plt.title('Training time for each model in sec', fontsize = 15)
+# plt.ylim(0,20)
+plt.xticks(rotation=90)
+plt.show()
+
+
+#! Train architecture with the best result
+# Split into train/test datasets using all of the pictures
+train_df,test_df = train_test_split(df, test_size=0.1, random_state=0)
+
+# Create the generator
+train_generator,test_generator,train_images,val_images,test_images=create_gen()
+
+# Create and train the model
+model = get_model(tf.keras.applications.DenseNet201)
+history = model.fit(train_images, validation_data=val_images, epochs=5, callbacks=[tf.keras.callbacks.BackupAndRestore(backup_dir="/tmp/backup", save_freq='epoch', delete_checkpoint=True),tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=1, restore_best_weights=True)])
+model.save("C:/Users/nsant/OneDrive/Documents/Uni/Y3/Food_Photogrammetry/Code/foodRecognitionClassifier.h5")
+model.save("C:/Users/nsant/OneDrive/Documents/Uni/Y3/Food_Photogrammetry/Code/foodRecognitionClassifier.keras")
+import joblib # save model with joblib 
+filename = 'C:/Users/nsant/OneDrive/Documents/Uni/Y3/Food_Photogrammetry/Code/foodRecognitionClassifier.sav'
+joblib.dump(model, filename)
+
+import pickle
+model_pkl_file="C:/Users/nsant/OneDrive/Documents/Uni/Y3/Food_Photogrammetry/Code/foodRecognitionClassifier.pkl"
+with open(model_pkl_file, 'wb') as file:  
+    pickle.dump(model, file)
+
+pd.DataFrame(history.history)[['accuracy','val_accuracy']].plot()
+plt.title("Accuracy")
+plt.show()
+
+pd.DataFrame(history.history)[['loss','val_loss']].plot()
+plt.title("Loss")
+plt.show()
+
+# Predict the label of the test_images
+predict = model.predict(test_images)
+predict = np.argmax(predict,axis=1)
+
+# Map the label
+labels = (train_images.class_indices)
+labels = dict((v,k) for k,v in labels.items())
+predict = [labels[k] for k in predict]
+
+# Get the accuracy on the test set
+y_test = list(test_df.fruit)
+acc = accuracy_score(y_test,predict)
+print(f'# Accuracy on the test set: {acc * 100:.2f}%')
+
+sampleImage1="C:\\Users\\nsant\\OneDrive\\Documents\\Uni\\Y3\\Project_MMME3083\\Code\\apple.jpg"
+y_predict = model.predict(sampleImage1)
+print("accuracy with apple.jpg \n",accuracy_score(y_test, y_predict))
+bapple="C:\\Users\\nsant\\OneDrive\\Documents\\Uni\\Y3\\Project_MMME3083\\Code\\apple_1.jpg"
+y_predict = model.predict(bapple)
+print("accuracy with apple_1.jpg \n",accuracy_score(y_test, y_predict)) 
+mango="C:\\Users\\nsant\\OneDrive\\Documents\\Uni\\Y3\\Project_MMME3083\\Code\\mango.jpg"
+y_predict = model.predict(mango)
+print("accuracy with mango.jpg \n",accuracy_score(y_test, y_predict)) 
+owang="C:\\Users\\nsant\\OneDrive\\Documents\\Uni\\Y3\\Project_MMME3083\\Code\\orange.jpg"
+y_predict = model.predict(owang)
+print("accuracy with orange.jpg \n", accuracy_score(y_test, y_predict)) 
+
+# Display a confusion matrix
+from sklearn.metrics import confusion_matrix
+cf_matrix = confusion_matrix(y_test, predict, normalize='true')
+plt.figure(figsize = (10,7))
+sns.heatmap(cf_matrix, annot=False, xticklabels = sorted(set(y_test)), yticklabels = sorted(set(y_test)),cbar=False)
+plt.title('Normalized Confusion Matrix', fontsize = 23)
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=15)
+plt.show()
+
+# Display picture of the dataset with their labels
+fig, axes = plt.subplots(nrows=4, ncols=6, figsize=(20, 12),subplot_kw={'xticks': [], 'yticks': []})
+
+for i, ax in enumerate(axes.flat):
+    ax.imshow(plt.imread(test_df.path.iloc[i]))
+    ax.set_title(f"True: {test_df.fruit.iloc[i].split('_')[0]}\nPredicted: {predict[i].split('_')[0]}", fontsize = 15)
+plt.tight_layout()
+plt.show()

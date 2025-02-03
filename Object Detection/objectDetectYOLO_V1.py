a40e767ad7ebeef3172f826e93be6a102cda6551
NoahSantaub <nsantaub@gmail.com>
2025-02-03 08:42:46 +0000

    Successful Single Object Prediction

--- Object Detection/objectDetectYOLO_V1.py
+++ Object Detection/objectDetectYOLO_V1.py
@@ -1,7 +1,10 @@
 import cv2 as cv
-from PIL import Image
-from ultralytics import YOLO
 import numpy
+from ultralytics import YOLO
+# Docs & Resources used 
+# https://docs.ultralytics.com/modes/predict/ # https://docs.ultralytics.com/reference/engine/results/
+# https://core-electronics.com.au/guides/raspberry-pi/getting-started-with-yolo-object-and-animal-recognition-on-the-raspberry-pi/
+
 testFruitArray=["Image Recognition\\Test Images\\apple.jpg", "Image Recognition\\Test Images\\apple_1.jpg", "Image Recognition\\Test Images\\mango.jpg",
                 "Image Recognition\\Test Images\\orange.jpg", "Project_MMME3083\\Code\\Fruit Image DB\\Orange\\Orange0016.png"]
 testFruit=testFruitArray[1]
@@ -9,7 +12,6 @@ testFruit=testFruitArray[1]
 # Load a pretrained YOLO11n-cls Classify model
 model = YOLO("yolo11m.pt")
 
-## https://core-electronics.com.au/guides/raspberry-pi/getting-started-with-yolo-object-and-animal-recognition-on-the-raspberry-pi/
 imageRaw = cv.imread(testFruit)# Load image to be read
 #cv.imshow("Image", image)
 #cv.waitKey(0)# Wait for a key press
@@ -18,17 +20,26 @@ imageRaw = cv.imread(testFruit)# Load image to be read
 results = model(imageRaw)
 
 # Output the visual detection data, we will draw this on our camera preview window
-annotated_image = results[0]#.plot()
+annotated_image = results[0].plot()
+cv.imshow("Camera", annotated_image)
+cv.waitKey(0)
 
 predictedClass = results[0].boxes.cls.numpy()
 predictionConfidance = results[0].boxes.conf.numpy()
 
-probs = results[0].probs  # get classification probabilities
-top1_confidence = probs.top1conf  # get confidence of top 1 class
-print(f"Top 1 class confidence: {top1_confidence.item():.4f}")
+strippedClassID = str(predictedClass).replace('[','').replace(']','').replace(' ', '')
+className=model.names.get(int(strippedClassID))
+strippedClassConfidance = str(predictionConfidance).replace('[','').replace(']','').replace(' ', '')
+
+print("Predicted Class: ",className,
+    "\nPrediction Confidance: ",strippedClassConfidance)
 
 
-print("Predicted Class: ",predictedClass,"\nPrediction Confidance: ",predictionConfidance)
+boxes = results[0].boxes  # Boxes object for bounding box outputs
+masks = results[0].masks  # Masks object for segmentation masks outputs
+keypoints = results[0].keypoints  # Keypoints object for pose outputsprint
+probs = results[0].probs  # Probs object for classification outputs
+obb = results[0].obb  # Oriented boxes object for OBB outputs
 
 """
 # Run inference on an image

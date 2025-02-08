8629f9d89c2e2d8e2b3b33cc040558f385e53ef2
NoahSantaub <nsantaub@gmail.com>
2025-02-08 01:02:09 +0000

    Transfer Learning

--- Object Detection/ImageScraper.py
+++ Object Detection/ImageScraper.py
@@ -2,9 +2,12 @@ import fiftyone as fo
 import fiftyone.zoo as foz
 
 # Define the classes you want to include
-fruitList = ["Apple","Banana","Mango","Orange", "Pear", "Peach",]
-#fruitList = ["Apple","Banana","Grapefruit","Lemon","Mango","Orange", "Pear", "Peach", , "Watermelon", "Tomato", "Strawberry"]
-# 
+#fruitList = ["Apple","Banana","Grapefruit","Lemon","Mango","Orange", "Peach", "Pear", "Watermelon", "Tomato", "Strawberry"]
+fruitList = ["Apple", "Grape", "Common fig", "Pear", "Strawberry" , "Tomato", "Lemon", "Banana", "Orange", "Peach", "Pear", "Mango", "Pineapple", "Grapefruit", "Pomegranate", "Watermelon", "Cantaloupe"]
+# Fruits in Open Images v7 - "Apple", "Grape", "Common fig", "Pear", "Strawberry" , "Tomato", "Lemon", "Banana, "Orange", "Peach", "Pear", "Mango", "Pineapple", "Grapefruit", "Pomegranate", "Watermelon", "Cantaloupe"
+# Veg in Open Images v7 - "Artichoke", "Asparagus", "Bell pepper", "Broccoli", "Cabbage", "Carrot", "Cucumber", "Mushroom", "Potato", "Pumpkin", "Radish", "Salad", "Winter melon", "Zuccini"
+
+
 # Images that contain all `label_types` and `classes` will be
 # prioritized first, followed by images that contain at least one of
 # the required `classes`. If there are not enough images matching
@@ -13,18 +16,35 @@ fruitList = ["Apple","Banana","Mango","Orange", "Pear", "Peach",]
 # Detections - 
 # Classifications - 
 # Segmentations - 
+
+# The splits to export
+splits = ["train", "test", "validation"]
+
 dataset = foz.load_zoo_dataset(
     "open-images-v7",
-    #split="train", # specify splits to load; is none provided all splits are loaded 
-    label_types=["detections","classifications"], # label types to load - ("detections", "classifications", "relationships", "points", segmentations") - defult all labels loaded
+    #split=splits, #"train", # specify splits to load, ('train', 'test', 'validation'); is none provided all splits are loaded 
+    label_types=["detections"], # yolo only supports detections label # label types to load - ("detections", "classifications", "relationships", "points", segmentations") - defult all labels loaded
     classes=fruitList,
-    max_samples=1000,  # manimum number of samples to load per split
+    max_samples=17000, #estimating 1000 images per class # maximum number of samples to load per split
     seed=0,# ensures repeatability
 )
 
-# Export the dataset to COCO format
-dataset.export(
-    export_dir="C:/Users/nsant/OneDrive/Documents/Uni/Y3/Project_MMME3083/Code/open-images-v7-COCO",
-    dataset_type=fo.types.COCODetectionDataset, # export the data into the coco format
-)
-session = fo.launch_app(dataset.view(), port=5151)
+export_dir="C:/Users/nsant/OneDrive/Documents/Uni/Y3/Project_MMME3083/Code/open-images-v7-COCO-v04" #export_dir = "/path/for/yolov5-dataset"
+label_field = "ground_truth"  # for example
+
+# The dataset or view to export
+# We assume the dataset uses sample tags to encode the splits to export
+#dataset_or_view = fo.load_dataset(dataset)
+
+# Export the splits
+for split in splits:
+    split_view = dataset.match_tags(split)
+    split_view.export(
+        export_dir=export_dir,
+        dataset_type=fo.types.YOLOv5Dataset,
+        #label_field=label_field,
+        split=split,
+        classes=fruitList,# All splits must use the same classes list
+    )
+
+#session = fo.launch_app(dataset.view(), port=5151)
--- Object Detection/objectDetectYOLO_V2_MultipleObject.py
+++ Object Detection/objectDetectYOLO_V2_MultipleObject.py
@@ -1,6 +1,15 @@
 import cv2 as cv
 import numpy
 from ultralytics import YOLO
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
+
+import torch
+if torch.cuda.is_available():
+    print("gpu detected")
+else:
+    print("gpu not detected!")
+    exit()
 # Docs & Resources used 
 # https://docs.ultralytics.com/modes/predict/ # https://docs.ultralytics.com/reference/engine/results/
 # https://core-electronics.com.au/guides/raspberry-pi/getting-started-with-yolo-object-and-animal-recognition-on-the-raspberry-pi/
@@ -11,11 +20,35 @@ testFruitArray=["Image Recognition\\Test Images\\apple.jpg", "Image Recognition\
                 "Image Recognition\\Test Images\\Multi_4.jpg", "Image Recognition\\Test Images\\Multi_5.jpg"]
 testFruit=testFruitArray[7]
 
-# Load a pretrained YOLO11n-cls Classify model
-model = YOLO("yolo11m.pt")
-results = model.train(data="C:/Users/nsant/OneDrive/Documents/Uni/Y3/Project_MMME3083/Code/open-images-v7-COCO/labels.json", epochs=100, imgsz=640)
-
-model.save("yolo11m-TransferLearningV0.1.pt")
+# Load a pretrained model
+model = YOLO("yolo11n.pt")
+#model.to('cuda')
+results = model.train(data="C:/Users/nsant/OneDrive/Documents/Uni/Y3/Project_MMME3083/Code/open-images-v7-COCO-v04\dataset.yaml", epochs=300, imgsz=640,workers=0, batch=-1, patience=5, optimizer="auto", 
+lr0= 0.00269,
+lrf= 0.00288,
+momentum= 0.73375,
+weight_decay= 0.00015,
+warmup_epochs= 1.22935,
+warmup_momentum= 0.1525,
+box= 18.27875,
+cls= 1.32899,
+dfl= 0.56016,
+hsv_h= 0.01148,
+hsv_s= 0.53554,
+hsv_v= 0.13636,
+degrees= 0.0,
+translate= 0.12431,
+scale= 0.07643,
+shear= 0.0,
+perspective= 0.0,
+flipud= 0.0,
+fliplr= 0.08631,
+mosaic= 0.42551,
+mixup= 0.0,
+copy_paste= 0.0)
+#batch=-1 automatically determines the batch size that can be efficiently processed based on your device's capabilities
+#patience=5 # training will stop if there's no improvement in validation metrics for 5 consecutive epochs
+model.save("yolo11n-TransferLearningV04-OptimisedHyperparameters.pt")
 
 imageRaw = cv.imread(testFruit)# Load image to be read
 #cv.imshow("Image", imageRaw), cv.waitKey(0)# output original image # Wait for a key press
